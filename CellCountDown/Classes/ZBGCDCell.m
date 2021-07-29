0c1fd4cbefa300de8bbb6ee80629eedc7f8d9adb
biyuhuaping <biyuhuaping@163.com>
2021-07-29 16:19:52 +0800

    no message

--- CellCountDown/Classes/ZBGCDCell.m
+++ CellCountDown/Classes/ZBGCDCell.m
@@ -15,6 +15,7 @@
 @interface ZBGCDCell ()
 
 @property (copy, nonatomic) NSString *countdownIdenfier;
+@property (assign, nonatomic) NSInteger countDown;
 
 @end
 
@@ -41,6 +42,7 @@
     //取消原来的定时器
     [ZBGCDTimer canelTimer:self.countdownIdenfier];
     
+    self.countDown = model.count;
     WEAKSELF
     [ZBGCDTimer timerWithName:self.countdownIdenfier start:0 interval:1 repeats:YES async:NO action:^{
         [weakSelf updateCountDown:1];
@@ -53,20 +55,18 @@
     if (0) {
         return;
     }
-    /// 计算倒计时
-    ZBModel *model = self.model;
-    NSInteger countDown = model.count - timeInterval;
+
     /// 当倒计时到了进行回调
-    if (countDown <= 0) {
+    if (self.countDown <= 0) {
         self.detailTextLabel.text = @"活动开始";
         // 回调给控制器
         if (self.countDownZero) {
-            self.countDownZero(model);
+            self.countDownZero(self.model);
         }
         return;
     }
     /// 重新赋值
-    self.detailTextLabel.text = [NSString stringWithFormat:@"%02zd:%02zd:%02zd", countDown/3600, (countDown/60)%60, countDown%60];
+    self.detailTextLabel.text = [NSString stringWithFormat:@"%02zd:%02zd:%02zd", self.countDown/3600, (self.countDown/60)%60, self.countDown%60];
 }
 
 - (void)dealloc{
